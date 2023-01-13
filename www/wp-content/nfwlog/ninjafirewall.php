ddc93776bca1ac1e4d067c049f4d42b6d20e670d
slash <dev@slash.builders>
2023-01-13 17:33:29 +0900

    archiving

---
+++ www/wp-content/nfwlog/ninjafirewall.php
@@ -0,0 +1,9 @@
+<?php
+// ===============================================================//
+// NinjaFirewall's loader.                                        //
+// DO NOT alter or remove it as long as NinjaFirewall is running! //
+// ===============================================================//
+if ( file_exists('/baro090611/www/wp-content/plugins/ninjafirewall/lib/firewall.php') ) {
+	@include_once '/baro090611/www/wp-content/plugins/ninjafirewall/lib/firewall.php';
+}
+// EOF
---
+++ www/wp-content/nfwlog/updates.php
@@ -0,0 +1,16 @@
+<?php exit; ?>
+[17/9월/22:13:23:24 +0900] No security rules update available.
+[18/9월/22:00:54:50 +0900] No security rules update available.
+[18/9월/22:05:38:44 +0900] No security rules update available.
+[18/9월/22:08:57:34 +0900] No security rules update available.
+[19/9월/22:04:34:50 +0900] No security rules update available.
+[19/9월/22:08:08:47 +0900] No security rules update available.
+[19/9월/22:17:57:19 +0900] No security rules update available.
+[20/9월/22:05:33:32 +0900] No security rules update available.
+[20/9월/22:09:50:22 +0900] No security rules update available.
+[20/9월/22:21:18:04 +0900] No security rules update available.
+[20/9월/22:21:59:17 +0900] No security rules update available.
+[20/9월/22:23:45:14 +0900] No security rules update available.
+[21/9월/22:10:03:18 +0900] No security rules update available.
+[22/9월/22:13:34:13 +0900] No security rules update available.
+[22/9월/22:13:34:13 +0900] No security rules update available.
